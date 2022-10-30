6d016f07bbfa48c43ff25adf8a7011cf63437e3f
Tvone123 <115849711+Tvone123@users.noreply.github.com>
2022-10-30 21:36:03 +0700

    Add files via upload

---
+++ MODUL2 TIMOTIUS.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <style>
+        body {
+            font-family:"Trebuchet MS",sans-serif;
+        }  
+        
+    </style>
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" >
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarNav">
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/booking.php">Booking</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+<div style="padding: 1% 6%;">
+    <h3 class="text-center">WELCOME TO EAD RENT</h3>
+    <h5 class="bg-dark mx-auto text-white text-center p-3">Find your deal</h5>
+    <div class="row w-75 m-auto" >
+            
+           <div class="col-4 px-2">
+                <div class="card" >
+                    <img src="https://tse2.mm.bing.net/th?id=OIP.6BiJMAwSYaMfo0zGp7e8owHaEU&pid=Api&P=0" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text">INNOVA</h5>
+                        <p class="card-text">6 KURSI</p>
+                        <p class="card-text">2300 cc</p>
+                        <p class="card-text">Manual</p>
+                    </div>
+
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary" href='/booking.php?number=1'>Book</a>
+                    </div>
+                    </div>
+                </div>
+        
+            <div class="col-4 px-2">
+                <div class="card" >
+                    <img src="https://www.team-bhp.com/forum/attachments/indian-car-scene/2106530d1610357107-rumour-next-gen-toyota-fortuner-coming-2022-fortunerexteriorrightfrontthreequarter19.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text">Fortuner</h5>
+                        <p class="card-text ">7 Kursi</p>
+                        <p class="card-text ">2393 cc</p>
+                        <p class="card-text">Manual/automatic</p>
+                        
+                    </div>
+                    
+                    <div class="card-footer text-center">
+                    <a class="btn btn-primary" href='/booking.php?number=2'>Book</a>
+                    </div>
+               
+                </div>
+            </div>
+          
+            <div class="col-4 px-2">
+                <div class="card" >
+                    <img src="https://tse3.mm.bing.net/th?id=OIP.4Mo92VdkU21kBNvcl1GdFQHaEQ&pid=Api&P=0" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text">Avanza</h5>
+                        <p class="card-text ">6 kursi</p>
+                        <p class="card-text ">1496 CC</p>
+                        <p class="card-text">Manual/Automatic</p>
+                    </div>
+                   
+                    <div class="card-footer text-center">
+                    <a class="btn btn-primary" href='/booking.php?number=3'>Book</a>
+                    </div>  
+                </div>
+            </div>
+            <div class="col-4 px-2">
+                <div class="card" >
+                    <img src="https://tse4.mm.bing.net/th?id=OIP.YjMlZoxuTNxixPDhg9IqXwHaEK&pid=Api&P=0" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text">Alphard</h5>
+                        <p class="card-text">7 KURSI</p>
+                        <p class="card-text">3456 cc</p>
+                        <p class="card-text">Automatic</p>
+                    </div>
+
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary" href='/booking.php?number=1'>Book</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-4 px-2">
+                    <div class="card" >
+                        <img src="https://tse3.mm.bing.net/th?id=OIP.jMYPiDoYcwAcpy7on-gI-AHaEK&pid=Api&P=0" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-text">Honda Brio</h5>
+                            <p class="card-text">4 KURSI</p>
+                            <p class="card-text">1199 cc</p>
+                            <p class="card-text">Manual/Automatic</p>
+                        </div>
+    
+                        <div class="card-footer text-center">
+                            <a class="btn btn-primary" href='/booking.php?number=1'>Book</a>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-4 px-2">
+                        <div class="card" >
+                            <img src="https://tse4.mm.bing.net/th?id=OIP.3UVBhCEEDYf5BBi759rJxQHaEq&pid=Api&P=0" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-text">Panther</h5>
+                                <p class="card-text">6 KURSI</p>
+                                <p class="card-text">2499 cc</p>
+                                <p class="card-text">Manual</p>
+                            </div>
+        
+                            <div class="card-footer text-center">
+                                <a class="btn btn-primary" href='/booking.php?number=1'>Book</a>
+                            </div>
+                            </div>
+                        </div>
+        </div>
+        </div>
+    
+    <footer class=" text-center bg-grey text-black">created by Timotius Carlos Simbolon_1202200318</footer>
+
+ 
+
+</body>
+</html>
